304965fe2d67d620eef859d4413545c0463eb060
Emre Can Karataş <87018026+blackstonecan@users.noreply.github.com>
2023-11-30 04:11:07 +0300

    Update team.js

--- src/routers/team.js
+++ src/routers/team.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const { create, get, remove } = require("../controllers/team");
+const { create, get, remove, update } = require("../controllers/team");
 const { controlAdminToken, controlMD5 } = require("../middlewares/authorization/auth");
 
 const router = express.Router();
 
 router.post("/", controlAdminToken, create);
 router.get("/:id", controlMD5, get);
+router.put("/", controlAdminToken, update);
 router.delete("/:id", controlAdminToken, remove);
 
 module.exports = router;
